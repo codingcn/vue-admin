d7866d26da5687dd30c71ecb490a9ac06f910fb3
Alan <20874823@qq.com>
2017-09-29 22:45:18 +0800

    l

--- src/components/page/Article.vue
+++ src/components/page/Article.vue
@@ -82,10 +82,6 @@
         },
         upload_action_cover: this.$difines.root_url + '/api/admin/article/upload-cover',
         file_list: [
-          {
-            name: 'food.jpeg',
-            url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-          }
         ],
         upload_action_editor: this.$difines.root_url + '/api/admin/article/upload-editor'   // 图片上传服务器地址
       }
